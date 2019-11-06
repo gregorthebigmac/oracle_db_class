9ba43e3544358734147cc6d1cf8b9879a7b983ac
Michaelveli <mjw166@gmail.com>
2019-11-06 10:42:43 -0600

    syntax errors

--- sales.sql
+++ sales.sql
@@ -6,14 +6,14 @@ create table sales_test (
 	keyword_1_hits int,
 	keyword_2_hits int,
 	keyword_3_hits int,
-	keyword_4 hits int,
-	keyword_5 hits int,
-	keyword_6 hits int,
+	keyword_4_hits int,
+	keyword_5_hits int,
+	keyword_6_hits int,
 	views_sales_ratio float,
-	similar_product_after_viewing_1 bigint references products(product_id)
-	similar_product_after_viewing_2 bigint references products(product_id)
-	similar_product_after_viewing_3 bigint references products(product_id)
-	similar_product_after_viewing_4 bigint references products(product_id)
-	similar_product_after_viewing_5 bigint references products(product_id)
-	similar_product_after_viewing_6 bigint references products(product_id)
+	similar_product_after_viewing_1 bigint references products(product_id),
+	similar_product_after_viewing_2 bigint references products(product_id),
+	similar_product_after_viewing_3 bigint references products(product_id),
+	similar_product_after_viewing_4 bigint references products(product_id),
+	similar_product_after_viewing_5 bigint references products(product_id),
+	similar_product_after_viewing_6 bigint references products(product_id),
 )
