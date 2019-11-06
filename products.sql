8855ebf25ec609473d5ddf7780c9c9ef277b50c5
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-06 11:02:47 -0600

    fixed syntax errors in products and sales

--- products.sql
+++ products.sql
@@ -5,20 +5,20 @@ create table product_test (
 	item_shipping decimal(19,4),
 	item_specs text,
 	star_char char(1) not null,
-	star_value decimal (1,3), not null,
-	/* TODO: add sub table for search keywords */
+	star_value decimal(19,4) not null,
+	/* TODO: add table for search keywords */
 	keyword_1 char(20),
 	keyword_2 char(20),
 	keyword_3 char(20),
 	keyword_4 char(20),
 	keyword_5 char(20),
 	keyword_6 char(20),
-	/* TODO: add sub table for customer reviews */
-	/* TODO: add sub table for related products */
-	related_product_1 int references products(product_id),
-	related_product_2 int references products(product_id),
-	related_product_3 int references products(product_id),
-	related_product_4 int references products(product_id),
-	related_product_5 int references products(product_id),
-	related_product_6 int references products(product_id),
+	/* TODO: add table for customer reviews */
+	/* TODO: add table for related products */
+	related_product_1 int references product_test(product_id),
+	related_product_2 int references product_test(product_id),
+	related_product_3 int references product_test(product_id),
+	related_product_4 int references product_test(product_id),
+	related_product_5 int references product_test(product_id),
+	related_product_6 int references product_test(product_id),
 );
