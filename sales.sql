68d7febb52018a6c6f31a660158f515338ed3e1f
Michaelveli <mjw166@gmail.com>
2019-11-06 16:55:44 -0600

    changed bigints to int, and shortened similar products to sim

--- sales.sql
+++ sales.sql
@@ -1,5 +1,5 @@
 create table sales_test (
-	product_id bigint primary key,
+	product_id int primary key,
 	qty_sold int,
 	revenue_from_product decimal(19,4),
 	num_of_customers_who_bought int,
@@ -10,10 +10,10 @@ create table sales_test (
 	keyword_5_hits int,
 	keyword_6_hits int,
 	views_sales_ratio float,
-	similar_product_after_viewing_1 bigint references product_test(product_id),
-	similar_product_after_viewing_2 bigint references product_test(product_id),
-	similar_product_after_viewing_3 bigint references product_test(product_id),
-	similar_product_after_viewing_4 bigint references product_test(product_id),
-	similar_product_after_viewing_5 bigint references product_test(product_id),
-	similar_product_after_viewing_6 bigint references product_test(product_id
+	sim_product_after_viewing_1 int references product_test(product_id),
+	sim_product_after_viewing_2 int references product_test(product_id),
+	sim_product_after_viewing_3 int references product_test(product_id),
+	sim_product_after_viewing_4 int references product_test(product_id),
+	sim_product_after_viewing_5 int references product_test(product_id),
+	sim_product_after_viewing_6 int references product_test(product_id)
 );
