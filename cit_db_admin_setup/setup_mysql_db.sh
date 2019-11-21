8574a0c003f2703991dca9a150a20fd35a9e54ab
gregorthebigmac <gregorthebigmac@gmail.com>
2019-11-20 20:44:30 -0600

    added .gitignore and finished the assignment

--- cit_db_admin_setup/setup_mysql_db.sh
+++ cit_db_admin_setup/setup_mysql_db.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-boo
+proceed=0
 echo "Creating Database..."
+# if (file exists) {
+#	break;
+#	if (log_dir exists) {
+#		if (~/mysql exists) {
+#			mkdir ~/mysql/log
+#		else {
+#			mkdir ~/mysql
+#			mkdir ~/mysql/log
+#		}
+#		touch ~/mysql/log/db_setup.log
+#	}
+#	else mkdir ~/mysql/log
+# else touch ~/mysql/log/db_setup.log
+
+
+
 if mkdir ~/mysql; then
 	if mkdir ~/mysql/log; then
 		if touch ~/mysql/log/db_setup.log; then
 			
+		else 
+		fi
+	else 
+	fi
+	
 echo "All output will be logged to ~/"
 read -n 1 -s -r -p "Press any key to continue"
