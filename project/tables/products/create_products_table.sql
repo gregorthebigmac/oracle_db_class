a660816a32ff473f1ff89eee9f9c35d6d9a7f7d3
gray fox <gfox@sql-srv.outerheaven>
2019-12-01 06:57:38 -0600

    all the scripts are working! Now we just need to fulfill the project reqs and we're g2g!

--- project/tables/products/create_products_table.sql
+++ project/tables/products/create_products_table.sql
@@ -1,19 +1,20 @@
+/* NOTE: varchar2 is SPECIFIC TO ORACLE DB and will not work in MySQL */
 connect amazon_db;
 create table product_test (
 	product_id			int primary key,
-	item_description	varchar2(10000) not null,
+	item_description	varchar(10000) not null,
 	item_price 			decimal(19,4) not null,
 	item_shipping		decimal(19,4),
-	item_specs 			varchar2(1000),
-	star_char 			varchar2(1) not null,
+	item_specs 			varchar(1000),
+	star_char 			varchar(1) not null,
 	star_value 			decimal(19,4) not null,
 	/* TODO: add table for search keywords */
-	keyword_1 varchar2(20),
-	keyword_2 varchar2(20),
-	keyword_3 varchar2(20),
-	keyword_4 varchar2(20),
-	keyword_5 varchar2(20),
-	keyword_6 varchar2(20),
+	keyword_1 varchar(20),
+	keyword_2 varchar(20),
+	keyword_3 varchar(20),
+	keyword_4 varchar(20),
+	keyword_5 varchar(20),
+	keyword_6 varchar(20),
 	/* TODO: add table for customer reviews */
 	/* TODO: add table for related products */
 	related_product_1 int references product_test(product_id),
@@ -32,16 +33,9 @@ insert into product_test values (
 	NULL,
 	'5',
 	'5.64',
-	'Gold,
-	"Frugal',
-	NULL,
-	NULL,
-	NULL,
+	'Gold',
+	'Frugal',
+	NULL, NULL,	NULL,
 	'Jewelry',
-	1,
-	2,
-	3,
-	4,
-	5,
-	6
+	1,2,3,4,5,6
 );
